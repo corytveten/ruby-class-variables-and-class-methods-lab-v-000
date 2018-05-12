b8dd518c5a9aedb3481a716642716fadcc77e421
corytveten <corytveten@gmail.com>
2018-05-12 18:53:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,10 +6,11 @@ class Song
 @@artists = []
 @@count = 0
 
+  attr_accessor :name
+  attr_accessor :artist
+  attr_accessor :genre
+
   def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
 
     @@count += 1
     @@artists << artist
